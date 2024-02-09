c112f82ff64ff2dee5dfe98551cce7c12566a27b
Facundo Ernesto Ortega <facuortega21@gmail.com>
2024-02-09 17:16:31 -0300

    creación del controller Perfil

---
+++ back/controllers/perfil/getById.php
@@ -0,0 +1,36 @@
+<?php
+require_once __DIR__ . '/../../models/entities/perfil.model.php';
+require_once __DIR__ . '/../../models/dtos/response/general-response.model.php';
+require_once __DIR__ . '/../../services/perfil.service.php';
+
+header('Content-Type: application/json');
+
+$response = new GeneralResponse();
+
+if (!isset($_GET['id'])) {
+    $response->tieneError = true;
+    $response->mensaje = "El id es requerido";
+    $json = json_encode($response);
+    echo $json;
+    return;
+}
+
+$id = $_GET['id'];
+
+try {
+    $perfilService = new PerfilService();
+    $perfil = $perfilService->getById($id);
+
+    $response->tieneError = false;
+    $response->resultado = $perfil;
+
+    if ($perfil == null) 
+        $response->mensaje = "No se encontró el perfil";
+} catch (Exception $e) {
+    $response->tieneError = true;
+    $response->mensaje = $e->getMessage();
+}
+
+$json = json_encode($response);
+echo $json;
+?>
---
+++ back/controllers/perfil/insert.php
@@ -0,0 +1,58 @@
+<?php
+require_once __DIR__ . '/../../models/entities/perfil.model.php';
+require_once __DIR__ . '/../../models/dtos/response/general-response.model.php';
+require_once __DIR__ . '/../../services/perfil.service.php';
+
+header('Content-Type: application/json');
+
+$response = new GeneralResponse();
+
+// Obtener datos del body
+$datosBody = json_decode(file_get_contents('php://input'), true);
+
+if (!isset($datosBody['descripcion'])) {
+    $response->tieneError = true;
+    $response->mensaje = "La descripción es requerida";
+    $json = json_encode($response);
+    echo $json;
+    return;
+}
+
+if (!isset($datosBody['fechaCreacion'])) {
+    $response->tieneError = true;
+    $response->mensaje = "La fecha de creación es requerida";
+    $json = json_encode($response);
+    echo $json;
+    return;
+}
+
+$descripcion = $datosBody['descripcion'];
+$fechaCreacion = $datosBody['fechaCreacion'];
+
+$perfil = new Perfil(
+    [
+        "id" => -1,
+        "descripcion" => $descripcion,
+        "fechaCreacion" => $fechaCreacion
+    ]
+);
+
+try {
+    $perfilService = new PerfilService();
+    $rowsAffected = $perfilService->insert($perfil);
+
+    $response->tieneError = false;
+    $response->resultado = $rowsAffected;
+
+    if ($rowsAffected == 0) 
+        $response->mensaje = "No se pudo crear el perfil " . $perfil->id . " " . $perfil->descripcion;
+    else
+        $response->mensaje = "Perfil creado exitosamente";
+} catch (Exception $e) {
+    $response->tieneError = true;
+    $response->mensaje = $e->getMessage();
+}
+
+$json = json_encode($response);
+echo $json;
+?>
