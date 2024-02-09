ff0bdaa91ec41e7fb0b627f136394bf740f87fb0
Facundo Ernesto Ortega <facuortega21@gmail.com>
2024-02-09 19:05:47 -0300

    se crea servicio de Pais

---
+++ back/controllers/pais/getAll.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../../models/entities/pais.model.php';
+require_once __DIR__ . '/../../models/dtos/response/general-response.model.php';
+require_once __DIR__ . '/../../services/pais.service.php';
+
+header('Content-Type: application/json');
+
+$response = new GeneralResponse();
+
+try {
+    $paisService = new PaisService();
+    $pais = $paisService->getAll();
+
+    $response->tieneError = false;
+    $response->resultado = $pais;
+
+    if ($pais == null) 
+        $response->mensaje = "No se encontraron paises";
+} catch (Exception $e) {
+    $response->tieneError = true;
+    $response->mensaje = $e->getMessage();
+}
+
+$json = json_encode($response);
+echo $json;
---
+++ back/services/pais.service.php
@@ -0,0 +1,63 @@
+<?php
+// Services
+require_once __DIR__ . '/database.service.php';
+require_once __DIR__ . '/../interfaces/data-access.interface.php';
+// Models
+require_once __DIR__ . '/../models/entities/pais.model.php';
+
+class PaisService implements DataAccess {
+    private DataBaseService $_dateBaseService;
+
+    public function __construct() {
+        $this->_dateBaseService = DataBaseService::getInstance();
+    }
+
+    public function getById($id) {
+        $query = "SELECT id, descripcion, codigo FROM paises WHERE id = ?";
+        $types = "i";
+        $params = [$id];
+
+        $resultData = $this->_dateBaseService->executeQuery($query, $types, $params);
+
+        if ($resultData != null) {
+            $pais = new Pais($resultData);
+            return $pais;
+        }
+
+        return null;
+    }
+
+    public function getByCode($codigo) {
+        $query = "SELECT id, descripcion, codigo FROM paises WHERE codigo = ?";
+        $types = "s";
+        $params = [$codigo];
+
+        $resultData = $this->_dateBaseService->executeQuery($query, $types, $params);
+
+        if ($resultData != null) {
+            $pais = new Pais($resultData);
+            return $pais;
+        }
+
+        return null;
+    }
+
+    public function getAll() {
+        $query = "SELECT id, descripcion, codigo FROM paises";
+        $types = "";
+        $params = [];
+
+        $resultData = $this->_dateBaseService->executeQueryArray($query, $types, $params);
+
+        if ($resultData != null) {
+            $paises = [];
+            foreach ($resultData as $data) {
+                $pais = new Pais($data);
+                array_push($paises, $pais);
+            }
+            return $paises;
+        }
+
+        return null;
+    }
+}
