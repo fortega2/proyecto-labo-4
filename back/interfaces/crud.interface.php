39827792f1f7324b54ec19d2590ee6f8b2097fcd
Facundo Ernesto Ortega <facuortega21@gmail.com>
2024-02-09 19:05:12 -0300

    creo nueva interfaz para obtener datos de la base

--- back/interfaces/crud.interface.php
+++ back/interfaces/crud.interface.php
@@ -1,7 +1,5 @@
 <?php
 interface CRUD {
-    public function getById(int $id);
-    public function getAll();
     public function insert(object $object);
     public function update(object $object);
     public function delete(int $id);
